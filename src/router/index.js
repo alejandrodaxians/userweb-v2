37830ff99fb83b66993068924cff153f319f192a
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-19 16:34:42 +0200

    Add each title page to tab

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Search from '../views/Search.vue'
+import Filter from '../views/Filter.vue'
+import Id from '../views/Id.vue'
+import Sede from '../views/Sede.vue'
+import Configure from '../views/Configure.vue'
+
 
 const router = createRouter ({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
-            component: Home
+            name: Search,
+            component: Search,
+            meta: {
+                title: 'Search'
+            }
         },
         {
-            path: '/about',
-            component: About
+            path: '/filter',
+            name: Filter,
+            component: Filter,
+            meta: {
+                title: 'Filter'
+            }
+        },
+        {
+            path: '/id',
+            name: Id,
+            component: Id,
+            meta: {
+                title: 'Id'
+            }
+        },
+        {
+            path: '/sede',
+            name: Sede,
+            component: Sede,
+            meta: {
+                title: 'Sede'
+            }
+        },
+        {
+            path: '/configure',
+            name: Configure,
+            component: Configure,
+            meta: {
+                title: 'Configure'
+            }
         }
     ]
 })
 
+router.beforeEach((toRoute, fromRoute, next) => {
+    window.document.title = toRoute.meta && toRoute.meta.title;
+  
+    next();
+  })
+  
 export default router
