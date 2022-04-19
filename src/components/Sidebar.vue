441ae7df17b1ae133ba6c1a9cfbc373fe272afb5
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-19 13:04:56 +0200

    Add storage to keep sidebar state

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,16 +9,36 @@
               <span class="material-icons">keyboard_double_arrow_right</span>
           </button>
       </div>
+
+      <h3>Menu</h3>
+      <div class="menu">
+          <router-link class="button" to="/">
+            <span class="material-icons">home</span>
+            <span class="text">Enlace1</span>
+          </router-link>
+          <router-link class="button" to="/about">
+            <span class="material-icons">visibility</span>
+            <span class="text">Enlace2</span>
+          </router-link>
+          <router-link class="button" to="/link">
+            <span class="material-icons">group</span>
+            <span class="text">Enlace3</span>
+          </router-link>
+          <router-link class="button" to="/linkle">
+            <span class="material-icons">email</span>
+            <span class="text">Enlace4</span>
+          </router-link>
+      </div>
   </aside>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const is_expanded = ref(false)
-
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 const ToggleMenu = () => {
-    is_expanded.value = !is_expanded.value
+	is_expanded.value = !is_expanded.value
+	localStorage.setItem("is_expanded", is_expanded.value)
 }
 </script>
 
@@ -72,15 +92,72 @@ aside {
         }
     }
 
+    h3, .button .text {
+        opacity: 0;
+        transition: 0.3s ease-out;
+    }
+
+    h3 {
+        color: var(--grey);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .menu {
+       margin: 0 -1rem; 
+
+       .button {
+           display: flex;
+           align-items: center;
+           text-decoration: none;
+           
+           padding: 0.5rem 1rem;
+           transition: 0.2s ease-out;
+
+           .material-icons {
+               font-size: 2rem;
+               color: var(--light);
+               transition: 0.2s ease-out;
+           }
+
+           .text {
+               color: var(--light);
+               transition: 0.2s ease-out;
+           }
+
+           &:hover, &.router-link-exact-active {
+               background-color: var(--dark-alt);
+
+               .material-icons, .text {
+                   color: var(--primary);
+               }
+           }
+
+           &.router-link-exact-active {
+               border-right: 5px solid var(--primary);
+           }
+       }
+    }
+
     &.is-expanded {
         width: var(--sidebar-width);
 
         .menu-toggle-wrap {
             top: -3rem;
-
             .menu-toggle {
                 transform: rotate(-180deg);
             }
+        }
+
+        h3, .button .text {
+        opacity: 1;
+    }
+    
+        .button {
+           .material-icons {
+               margin-right: 1rem;
+           } 
         }
     }
 
