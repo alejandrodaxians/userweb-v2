9b5664060389c53e9be28f9e41bfada26c99a965
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-20 15:45:26 +0200

    Add dynamic components for Create and Delete

--- src/views/Sede.vue
+++ src/views/Sede.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <!-- <CreateSede /> -->
-    <DeleteSede />
+    <div class="buttons">
+      <button class="crear" @click="component = 'CreateSede'">Crear Sede</button>
+      <button class="borrar" @click="component = 'DeleteSede'">Borrar Sede</button>
+    </div>
+    <transition name="fade" mode="out-in">
+    <component :is="component" />
+  </transition>
   </div>
 </template>
 
@@ -12,12 +17,52 @@
   export default {
     name: 'App',
     components: {
-      CreateSede,
-      DeleteSede
+      CreateSede, DeleteSede
+    },
+    data() {
+      return{
+        component: ''
+      }
     }
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0.1;
+}
+
+.buttons {
+  margin-left: 25px;
+}
+
+button {
+  height: 50px;
+  padding: 10px 20px;
+  margin: 15px;
+  color: var(--light);
+  border-radius: 20px; 
+  text-transform: uppercase;  
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;  
+    
+    &:hover {
+        border: solid 3px var(--dark);
+    }
+}
+
+.crear {
+  background: #04602c;
+}
+
+.borrar {
+    background: #c33535;
+}
 </style>
