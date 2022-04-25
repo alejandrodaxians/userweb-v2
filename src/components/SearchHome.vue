303c49372017542189792c4e6dadfbafa9faf5f8
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-25 13:03:18 +0200

    Add bidirectional sorting to table

--- src/components/SearchHome.vue
+++ src/components/SearchHome.vue
@@ -79,17 +79,26 @@ export default {
         }  
     },
     methods: {
-    "sortTable": function sortTable(col) {
-      this.rows.sort(function(a, b) {
-        if (a[col] > b[col]) {
-          return 1;
-        } else if (a[col] < b[col]) {
-          return  -1;
+        "sortTable": function sortTable(col) {
+            if (this.sortColumn === col) {
+                this.ascending = !this.ascending;
+            } else {
+                this.ascending = true;
+                this.sortColumn = col;
+            }
+
+            var ascending = this.ascending;
+
+            this.rows.sort(function(a, b) {
+                if (a[col] > b[col]) {
+                    return ascending ? 1 : -1
+                } else if (a[col] < b[col]) {
+                    return ascending ? -1 : 1
+                }
+            return 0;
+            })
         }
-        return 0;
-      })
-    }
-  },
+    },
 }
 </script>
 
