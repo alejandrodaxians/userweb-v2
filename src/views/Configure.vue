6a429df74b3f9ceb6c4e5f59b79184ace6d2fe2e
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-25 12:49:18 +0200

    Deleted Dropdwon import from Configure

--- src/views/Configure.vue
+++ src/views/Configure.vue
@@ -1,34 +1,14 @@
 <template>
-  <div id="app">
-    <header>
-      <Dropdown />
-    </header>
-  </div>  
+  <h1>Placeholder</h1>
 </template>
 
 <script>
-import Dropdown from '../components/Dropdown.vue'
 
 export default {
     name: 'App',
-    components: {
-      Dropdown
-    }
   }
 </script>
 
 <style lang="scss" scoped>
-  :root {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
 
-  header  {
-    width: 900px;
-    background-color: #222;
-    padding: 15px;
-    margin-left: 35px;
-    overflow: hidden;
-  }
 </style>
